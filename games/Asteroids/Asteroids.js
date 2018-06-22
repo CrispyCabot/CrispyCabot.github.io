6c183dd642174231441cd115e2ccbcc41bac5694
CrispyCabot <18431358+CrispyCabot@users.noreply.github.com>
2018-06-21 23:25:56 -0400

    Modifications to Asteroids game

--- games/Asteroids/Asteroids.js
+++ games/Asteroids/Asteroids.js
@@ -1,4 +1,5 @@
 var loc;
+var acc
 var right;
 var left;
 var up;
@@ -10,6 +11,9 @@ var r;
 var shotList = [];
 var obstacleList = [];
 var spaceCounter;
+var obstacleAmount;
+var time;
+var score;
 
 function preload() {
   ship = loadImage("spaceship.png");
@@ -26,6 +30,7 @@ function draw() {
   
   shotStuff();
   obstacleStuff();
+  displayStuff();
   
   moveShip();
   imageMode(CENTER);
@@ -52,25 +57,41 @@ function moveShip() {
     }
   }
   if (up) {
-    loc.x+=4*cos(radians(r));
-    loc.y-=4*sin(radians(r));
+    if (abs(acc.x) + abs(acc.y) < 5) {
+      acc.x+=.1*cos(radians(r));
+      acc.y-=.1*sin(radians(r));
+    }
   }
   if (down) {
-    loc.x-=4*cos(radians(r)); 
-    loc.y-=4*sin(radians(r));
-  }
-}
-
-function obstacleStuff() {
-  while (obstacleList.length < 4) {
-    var temp = new Obstacle();
-    append(obstacleList, temp);
-  }
-  for (var i=0; i<obstacleList.length;i++) {
-    obstacleList[i].update();
-    if (obstacleList[i].loc.x < 0 || obstacleList[i].loc.x > width || obstacleList[i].loc.y < 0 || obstacleList[i].loc.y > height) {
-      obstacleList.splice(i, 1);
+    if (abs(acc.x) + abs(acc.y) < 5) {
+      acc.x-=.1*cos(radians(r)); 
+      acc.y+=.1*sin(radians(r));
     }
+  }
+  if (loc.x < 0) {
+    loc.x = width;
+  }
+  if (loc.x > width) {
+    loc.x = 0;
+  }
+  if (loc.y > height) {
+    loc.y = 0;
+  }
+  if (loc.y < 0) {
+    loc.y = height;
+  }
+  loc.add(acc);
+  if (acc.x > 0) {
+    acc.x -= 0.01;
+  }
+  if (acc.y > 0) {
+    acc.y -= 0.01;
+  }
+  if (acc.x < 0) {
+    acc.x += 0.01;
+  }
+  if (acc.y < 0) {
+    acc.y += 0.01;
   }
 }
 
@@ -80,7 +101,7 @@ function shotStuff() {
       var acc = findAcc(r);
       var locHolder = createVector(loc.x, loc.y);
       append(shotList, [locHolder, acc]);
-      spaceCounter = 60;
+      spaceCounter = 20;
     }
     else {
       spaceCounter--;
@@ -89,25 +110,73 @@ function shotStuff() {
   else {
     spaceCounter--;
   }
-  for (var i=0; i < shotList.length; i++) {
+  for (var i=0; i < shotList.length; i++) {  //Loops through all the shots still in play
     var tempLoc = shotList[i][0];
     var tempAcc = shotList[i][1];
     tempLoc.add(tempAcc);
     stroke(255,255,0);
     strokeWeight(2);
-    line(tempLoc.x, tempLoc.y, tempLoc.x+4*tempAcc.x, tempLoc.y+4*tempAcc.y);
+    line(tempLoc.x, tempLoc.y, tempLoc.x+2*tempAcc.x, tempLoc.y+2*tempAcc.y);
     noStroke();
-    if (tempLoc.x < 0 || tempLoc.x > width || tempLoc.y < 0 || tempLoc.y > height) {
+    if (tempLoc.x < 0 || tempLoc.x > width || tempLoc.y < 0 || tempLoc.y > height) { //Check if shot went off screen
       shotList.splice(i, 1);
     }
   }
+}
+
+function obstacleStuff() {
+  while (obstacleList.length < 5) {
+    var temp = new Obstacle();
+    append(obstacleList, temp);
+  }
+  for (var i=0; i<obstacleList.length;i++) {
+    
+    var obstacle = obstacleList[i];
+    if (abs(obstacle.loc.x-loc.x) < 20 && abs(obstacle.loc.y - loc.y) < 20) { //Checks if obstacle hit ship *Only checks in a square from the midpoint 20 pixels horizontal and vertical*
+      noLoop();
+      textAlign(CENTER).textSize(50).fill(255,0,0);
+      text("Game Over", width/2, height/2);
+    }
+    
+    var dead = false;
+    
+    //Checks if obstacle went off screen
+   /* if (obstacleList[i].loc.x < 0 || obstacleList[i].loc.x > width || obstacleList[i].loc.y < 0 || obstacleList[i].loc.y > height) { //Checks if obstacle went off screen
+      dead = true;
+    }*/
+    if (obstacle.loc.x < 0) {
+      obstacle.loc.x = width;
+    }
+    if (obstacle.loc.x > width) {
+      obstacle.loc.x = 0;
+    }
+    if (obstacle.loc.y < 0) {
+      obstacle.loc.y = height;
+    }
+    if (obstacle.loc.y > height) {
+      obstacle.loc.y = 0;
+    }
+    
+    if (obstacleList[i].update() == 0) { //Returns 0 if obstacle was hit by shot
+      dead = true;
+    }
+    if (dead) {
+      obstacleList.splice(i, 1);
+    }
+  }
+  time = millis() / 1000
 }
 
 function findAcc(r) {
   var x = cos(radians(r));
   var y = -sin(radians(r));
   return createVector(8*x,8*y);
-}   
+}
+
+function displayStuff() {
+  textAlign(LEFT, TOP).textSize(40).fill(255,255,0);
+  text("Score: "+score, 15, 15);
+}
 
 function initV() { //Creates initial values
   right=false;
@@ -116,10 +185,14 @@ function initV() { //Creates initial values
   down=false;
   space=false;
   loc = createVector(width/2,height/2);
+  acc = createVector(0,0);
   r = 0.0;
   spaceCounter = 0;
   shotList = [];
   obstacleList=[];
+  obstacleAmount = 5;
+  time=0;
+  score=0;
 }
     
 function keyPressed() {
---
+++ games/Asteroids/Obstacle_Class.js
@@ -0,0 +1,88 @@
+function Obstacle() {
+  this.layer = round(random(1,3));
+  this.radius=200;
+  
+  var rand = int(random(1,4));
+  if (rand == 1) {
+    this.loc = createVector(0, random(0,height));
+    this.acc = createVector(random(1,2), random(-2,2));
+  }
+  if (rand == 2) {
+    this.loc = createVector(random(0,width), 0);
+    this.acc = createVector(random(-2,2), random(1,2));
+  }
+  if (rand == 3) {
+    this.loc = createVector(width, random(0,height));
+    this.acc = createVector(-random(1,2), random(-2,2));
+  }
+  if (rand == 4) {
+    this.loc = createVector(random(0,width), height);
+    this.acc = createVector(random(-2,2), -random(1,2));
+  }
+  
+  if (this.layer == 1) {
+    this.radius=20;
+  }
+  if (this.layer == 2) {
+    this.radius=40;
+  }
+  if (this.layer == 3) {
+    this.radius=80;
+  }
+  
+  this.update = function() {
+    this.loc.add(this.acc);
+    fill(255);
+    ellipse(this.loc.x, this.loc.y, this.radius, this.radius);
+    for (var m=0; m< shotList.length; m++) {
+      var temp = shotList[m][0];
+      if (dist(temp.x, temp.y, this.loc.x, this.loc.y) < this.radius) { //If a shot hit an asteroid
+        shotList.splice(m, 1);
+        
+        if (this.layer == 1) {
+          score+=80;
+          return 0;
+        }
+        if (this.layer == 2) {
+          score+=40;
+          var temp1 = new Obstacle()
+          temp1.setLocAccLayer(this.loc.x, this.loc.y, this.acc.x+1, this.acc.y+1, 1);
+          append(obstacleList, temp1);
+          var temp2 = new Obstacle();
+          temp2.setLocAccLayer(this.loc.x, this.loc.y, this.acc.x-1, this.acc.y-1, 1);
+          append(obstacleList, temp2);
+          return 0;
+        }
+        if (this.layer == 3) {
+          score+=20;
+          var temp1 = new Obstacle()
+          temp1.setLocAccLayer(this.loc.x, this.loc.y, this.acc.x+1, this.acc.y+1, 2);
+          append(obstacleList, temp1);
+          var temp2 = new Obstacle();
+          temp2.setLocAccLayer(this.loc.x, this.loc.y, this.acc.x-1, this.acc.y-1, 2);
+          append(obstacleList, temp2);
+          var temp3 = new Obstacle();
+          temp3.setLocAccLayer(this.loc.x, this.loc.y, this.acc.x-1.25, this.acc.y-1.25, 2);
+          append(obstacleList, temp3);
+          return 0;
+        }
+        
+      }
+    }
+  }
+  
+  this.setLocAccLayer = function(x, y, x1, y1, layer) {
+    this.loc.x = x;
+    this.loc.y = y;
+    this.acc.x = x1;
+    this.acc.y = y1;
+    this.layer = layer;
+    if (this.layer == 1) {
+      this.radius=20;
+    }
+    if (this.layer == 2) {
+      this.radius=40;
+    }
+  }
+}
+    
